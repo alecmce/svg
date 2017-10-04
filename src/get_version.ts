c6f93b4e23197cd165197448c885358653b126c9
Alec McEachran <alecmce@google.com>
2017-10-04 18:30:03 +0100

    adding prerequisites to the svg package.json

--- src/get_version.ts
+++ src/get_version.ts
@@ -1,7 +1,7 @@
 const MAJOR = 0;
 const MINOR = 0;
 const PATCH = 0;
-const BUILD = 4072;
+const BUILD = 4075;
 
 export default function getVersion(): string {
   return `v.${MAJOR}.${MINOR}.${PATCH}-${BUILD}`;
